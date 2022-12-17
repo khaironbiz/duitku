ccc3dc31d67ab62e1b42c0f57119a76bf1b7ffdf
khaironbiz <khaironbiz@gmail.com>
2022-12-17 09:17:05 +0700

    konfigurasi ulang koneksi ke db

--- auth/koneksi.php
+++ auth/koneksi.php
@@ -1,8 +1,17 @@
 <?php
-$servername = "localhost";
-$database   = "market";
-$username   = "root";
-$password   = "";
+$server_host = $_SERVER['SERVER_NAME'];
+if($server_host == "localhost") {
+    $servername = "localhost";
+    $database   = "market";
+    $username   = "root";
+    $password   = "";
+}else{
+    $servername = "localhost";
+    $database   = "tsicoid_market";
+    $username   = "tsicoid_khairon";
+    $password   = "@Sql250909#";
+}
+
 
 // Create connection
 
